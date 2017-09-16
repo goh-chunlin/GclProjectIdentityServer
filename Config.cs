ea43895c35df7c811330f3ab0917b63b2eb5e7bc
goh-chunlin <chunlin@live.com.sg>
2017-09-16 17:38:37 +0800

    Formatted Config.cs.

--- Config.cs
+++ Config.cs
@@ -4,58 +4,58 @@ using IdentityServer4;
 
 namespace GclProjectIdentityServer
 {
-    public class Config
-    {
+	public class Config
+	{
 		public static IEnumerable<IdentityResource> GetIdentityResources()
-        {
-            return new List<IdentityResource>
-            {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                new IdentityResources.Email(),
-            };
-        }
+		{
+			return new List<IdentityResource>
+			{
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile(),
+				new IdentityResources.Email(),
+			};
+		}
 
-        // scopes define the API resources in your system
-        public static IEnumerable<ApiResource> GetApiResources()
-        {
-            return new List<ApiResource>
-            {
-                new ApiResource("api1", "My API")
-            };
-        }
+		// scopes define the API resources in your system
+		public static IEnumerable<ApiResource> GetApiResources()
+		{
+			return new List<ApiResource>
+			{
+				new ApiResource("api1", "My API")
+			};
+		}
 
-        // client want to access resources (aka scopes)
-        public static IEnumerable<Client> GetClients(string domainName)
-        {
-            return new List<Client>
-            {
-                new Client
-                {
-                    ClientId = "client",
+		// client want to access resources (aka scopes)
+		public static IEnumerable<Client> GetClients(string domainName)
+		{
+			return new List<Client>
+			{
+				new Client
+				{
+					ClientId = "client",
 					ClientName = "MVC Client",
-                    AllowedGrantTypes = GrantTypes.Implicit,
+					AllowedGrantTypes = GrantTypes.Implicit,
 
 					RequireConsent = false,
 
-                    ClientSecrets = 
-                    {
-                        new Secret("secret")
-                    },
+					ClientSecrets =
+					{
+						new Secret("secret")
+					},
 
 					RedirectUris           = { $"{domainName}/signin-oidc" },
-    				PostLogoutRedirectUris = { $"{domainName}/signout-callback-oidc" },
+					PostLogoutRedirectUris = { $"{domainName}/signout-callback-oidc" },
 
-                    AllowedScopes = { 
+					AllowedScopes = {
 						IdentityServerConstants.StandardScopes.OpenId,
-        				IdentityServerConstants.StandardScopes.Profile,
-						"api1" 
+						IdentityServerConstants.StandardScopes.Profile,
+						"api1"
 					},
 
 					AllowOfflineAccess = true
 
-                }
-            };
-        }
-    }
+				}
+			};
+		}
+	}
 }
